205f3705396a395d380e37091167b9b6aba6b50c
Dominik Schmidt <dominikschmidt1893@gmail.com>
2018-01-21 21:51:27 -0800

    /

--- Program2/src/Words.java
+++ Program2/src/Words.java
@@ -1,12 +1,29 @@
 import java.util.Scanner;
-
+/**
+ * Created by domin on 1/21/2018.
+ */
 public class Words {
-    public static void main(String args[]){
+    public static void main(String args[]) {
+        int notRepeated = 0;
         Scanner kb = new Scanner(System.in);
         int numOfWords = kb.nextInt();
         String[] Words = new String[numOfWords];
-        for(int i =0; i < numOfWords; i++){
+        for (int i = 0; i < numOfWords; i++) {
             Words[i] = kb.next();
         }
+        for (int i = 0; i < numOfWords; i++){
+            for (int j = 0; j < Words[i].length(); j++){
+                char check = Words[i].charAt(j);
+                int index1 = Words[i].indexOf(check);
+                int index2 = Words[i].lastIndexOf(check);
+                if (index1 == index2){
+                    if (j==Words[i].length()-1){
+                        notRepeated++;
+
+                    }
+                }
+            }
+        }
+        System.out.print(notRepeated);
     }
 }
