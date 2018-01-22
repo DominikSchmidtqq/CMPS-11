5ba84dd2e2ab0e09ea3eb30bd4f3a1af7f14afda
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2018-01-21 17:14:20 -0800

    matrix fill
    */

--- Program2/src/Matrix.java
+++ Program2/src/Matrix.java
@@ -3,10 +3,15 @@
  */
 import java.util.*;
 public class Matrix {
-    public static void main(String args[]){
-        Scanner kb =  new Scanner(System.in);
+    public static void main(String args[]) {
+        Scanner kb = new Scanner(System.in);
         int rows = kb.nextInt();
         int columns = kb.nextInt();
-        int Matrix[][]= new int[rows][columns];
+        int Matrix[][] = new int[rows][columns];
+        for (int j = 0; j < rows; j++) {
+            for (int i = 0; i < columns; i++) {
+                Matrix[j][i] = kb.nextInt();
+            }
+        }
     }
 }
