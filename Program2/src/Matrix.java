449b1f407187e4c6905f5c5b21f385856f5b4e39
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2018-01-21 17:06:27 -0800

    created matrix class

---
+++ Program2/src/Matrix.java
@@ -0,0 +1,12 @@
+/**
+ * Created by domin on 1/21/2018.
+ */
+import java.util.*;
+public class Matrix {
+    public static void main(String args[]){
+        Scanner kb =  new Scanner(System.in);
+        int rows = kb.nextInt();
+        int columns = kb.nextInt();
+        int Matrix[][]= new int[rows][columns];
+    }
+}
