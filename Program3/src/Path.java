d6a5b521ae4fdcfc6e6a87d8e51e3b4d718ccfcc
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2018-02-05 20:38:02 -0800

    path update

--- Program3/src/Path.java
+++ Program3/src/Path.java
@@ -1,34 +1,58 @@
-import java.util.*;
+import java.awt.*;
 public class Path {
-    double length;
-    ArrayList<Integer> points;
+    Point[] points;
+    Point[] newArray;
+    Path path;
     Path(){
-        ArrayList<Integer>points = new ArrayList<Integer>();
-        this.points = points;
+        this.points = new Point[0];
     }
     public Path addPoint(int x, int y){
-        this.points.add(x);
-        this.points.add(y);
+        this.points[points.length-1] = new Point(x,y);
         return path;
     }
-    public boolean removePoint(int i){
-        int index = ((this.points.indexOf(i)+1)/2);
-        this.points.remove(index);
-        this.points.remove(index+1);
-        return true;
+    public boolean removePoint(int index){
+        if (index <= this.points.length - 1){
+            Point[] newArray = new Point[this.points.length-2];
+            for(int i = 0; i < index; i++){
+                newArray[i] = this.points[i];
+            }
+            for(int i = index; i < newArray.length; i++){
+                newArray[i] = this.points[i];
+            }
+            this.points = newArray;
+            return true;
+        }
+        else
+            return false;
     }
-    public void addPath(Path p){
-
+    void addPath(Path p){
+        int length = this.points.length + p.points.length;
+        newArray = new Point[length];
+        for (int i = 0; i < this.points.length; i++){
+            newArray[i] = this.points[i];
+        }
+        for (int i = this.points.length; i < p.points.length; i++){
+            newArray[i] = p.points[i];
+        }
     }
     public double getLength(){
-
+        double sum = 0;
+        for (int i = 0; i < this.points.length-2; i++){
+            sum += this.points[i].distance(this.points[i+1]);
+        }
+        return sum;
+    }
+    public double getDistance(){
+        return this.points[0].distance(this.points[this.points.length-1]);
     }
     boolean isLonger(Path p){
-
+        if (this.getDistance() > p.getDistance()){
+            return true;
+        }
+        else return false;
     }
-    public static void main(String args[]){
+    public static void main(String args []){
         Path path = new Path();
+        path.addPoint(0,0);
     }
-
-
 }
