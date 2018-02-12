20793416b97f64193003168164e503bc3a6a9ecf
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2018-02-12 15:22:56 -0800

    /*

--- Program3/src/Bingo/Card.java
+++ Program3/src/Bingo/Card.java
@@ -1,6 +1,7 @@
 /**
  * Created by domin on 2/5/2018.
  */
+package Bingo;
 public class Card {
     int[][] numbers;
     boolean[][] marked;
--- Program3/src/Bingo/Player.java
+++ Program3/src/Bingo/Player.java
@@ -1,49 +1,54 @@
+package Bingo;
+
 /**
- * Created by domin on 2/5/2018.
+ * Created by domin on 2/12/2018.
  */
 public class Player {
-    String name;
-    Card[] cards;
-    Player(String name, Card[] cards){
-        this.name = name;
-        this.cards = cards;
-    }
-    Player(){
-        this("", new Card[0]);
-    }
-    public String getName(){
-       // if (this.isWinner() == true)
+    /**
+     * Created by domin on 2/5/2018.
+     */
+        String name;
+        Card[] cards;
+        Player(String name, Card[] cards){
+            this.name = name;
+            this.cards = cards;
+        }
+        Player(){
+            this("", new Card[0]);
+        }
+        public String getName(){
+            // if (this.isWinner() == true)
             return this.name;
-        //return "";
-    }
-    public Card[] getCards(){
-        return this.cards;
-    }
-    public boolean isWinner() {
-        for (int j = 0; j < this.getCards().length; j++) {
-            for (int k = 0; k < 5; k++) {
-                for (int l = 0; l < 5; l++) {
-                    if (this.cards[j].marked[k][l] == false)
-                        break;
-                    else
-                        return true;
+            //return "";
+        }
+        public Card[] getCards(){
+            return this.cards;
+        }
+        public boolean isWinner() {
+            for (int j = 0; j < this.getCards().length; j++) {
+                for (int k = 0; k < 5; k++) {
+                    for (int l = 0; l < 5; l++) {
+                        if (this.cards[j].marked[k][l] == false)
+                            break;
+                        else
+                            return true;
 
+                    }
+                }
+                for (int k = 0; k < 5; k++) {
+                    for (int l = 0; l < 5; l++) {
+                        if (this.cards[j].marked[l][k] == false)
+                            break;
+                        else
+                            return true;
+                    }
                 }
             }
-            for (int k = 0; k < 5; k++) {
-                for (int l = 0; l < 5; l++) {
-                    if (this.cards[j].marked[l][k] == false)
-                        break;
-                    else
-                        return true;
-                }
+            return false;
+        }
+        public void markNumber(int number){
+            for (int i = 0; i < this.cards.length; i++){
+                cards[i].markNumber(number);
             }
         }
-        return false;
     }
-    public void markNumber(int number){
-        for (int i = 0; i < this.cards.length; i++){
-            cards[i].markNumber(number);
-        }
-    }
-}
